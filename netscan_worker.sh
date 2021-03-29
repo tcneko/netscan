538a2de750336102bd698c5497ce50e2885b1d0f
tcneko <tcneko@outlook.com>
2021-03-30 01:24:08 +0800

    v20210330_4

--- netscan_worker.sh
+++ netscan_worker.sh
@@ -18,9 +18,8 @@ netscan() {
   ix=1
   while true; do
     start_time=$(date +%s)
-    output=$(nmap -T${nmap_time_profile} -n -sn ${net} | grep "Nmap done" | cut -d" " -f3,6 | tr -d '(' | sed -E "s#([0-9]*) ([0-9]*)#${net} ${ix} \2 \1#g")
+    output=$(nmap -T${nmap_time_profile} -n -sn ${net} 2>/dev/null | grep "Nmap done" | cut -d" " -f3,6 | tr -d '(' | sed -E "s#([0-9]*) ([0-9]*)#${net} ${ix} \2 \1#g")
     echo ${output} >${f_pipe}
-    # kill -s USR1 $PPID
     end_time=$(date +%s)
     sleep_time=$((${rescan} - ${end_time} + ${start_time}))
     if ((${sleep_time} > 0)); then
